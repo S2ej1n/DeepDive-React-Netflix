ef92eca5330c0415156c1cf3ea5da6291654f58e
S2ej1n <ks320205@naver.com>
2025-03-17 16:28:31 +0900

    모달생성(1)

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from './MovieModal';
 
 const Row = ({isLargeRow, title, id, fetchUrl}) => {
     const [movies, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelected] = useState({});
 
     // 필요한 정보 가져오기
     useEffect(() =>{
@@ -14,6 +17,11 @@ const Row = ({isLargeRow, title, id, fetchUrl}) => {
         const request = await axios.get(fetchUrl);
         setMovies(request.data.results);
         return request;
+    }
+
+    const handleClick = (movie) => {
+      setModalOpen(true);
+      setMovieSelected(movie);
     }
 
   return (
@@ -38,6 +46,7 @@ const Row = ({isLargeRow, title, id, fetchUrl}) => {
                 isLargeRow ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.name}
               loading="lazy"
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -50,6 +59,13 @@ const Row = ({isLargeRow, title, id, fetchUrl}) => {
           </span>
         </div>
       </div>
+
+      {
+        modalOpen && (
+          <MovieModal {...movieSelected} 
+          setModalOpen={setModalOpen}/>
+        )
+      }
     </section>
   )
 }
