cb001ae2025c234fcf46520d3cd21ef52739ba95
S2ej1n <ks320205@naver.com>
2025-03-17 14:35:28 +0900

    gitignore 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import config from "./apikey";
 
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key : "",
+        api_key : config.API_KEY,
         language: "ko-KR",
     },
 });
