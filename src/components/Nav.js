4d65942f7f947dd007d6b59aa768ec78ed4a86c0
S2ej1n <ks320205@naver.com>
2025-03-21 14:38:41 +0900

    useLocation 통한 검색구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    // 이걸로 경로 이동시켜줌.
+    const navigate = useNavigate();
+
+    const handelChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    }
+
     
     useEffect(() => {
         window.addEventListener("scroll", () =>{
@@ -26,6 +36,14 @@ const Nav = () => {
             className='nav__logo'
             onClick={() => window.location.reload()}
         />
+
+        {/* 검색 */}
+        <input 
+        value={searchValue} 
+        onChange={handelChange} 
+        className='nav__input' type="text"
+        placeholder="영화를 검색해주세요" />
+
         <img
             alt="User logged"
             src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg"
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function SearchPage() {
+
+  const [searchResult, setSearchResult] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get('q'); //q로
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async(searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      )
+      setSearchResult(request.data.results);
+    }catch (error){
+      console.error(error);
+    }
+  }
+
   return (
     <div>
-      
     </div>
   )
 }
